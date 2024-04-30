6b9b93d0556d080d055da5725c036b5b3d9a6268
NagyKaroly92 <113894832+NagyKaroly92@users.noreply.github.com>
2024-04-30 15:13:50 +0200

    testapp, bll

---
+++ BusinessLayer/BusinessLayer/Mertekegyseg.cs
@@ -0,0 +1,36 @@
+﻿namespace BusinessLayer
+{
+    public class Mertekegyseg : IDisposable
+    {
+        public Mertekegyseg()
+        {
+                
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public string GetMertekegyseg()
+        {
+            string ret = string.Empty;
+
+            using (var v = new DAL.MyDbContextFactory().CreateDbContext(null))
+            {
+                v.MennyisegiEgysegs.Add(new DAL.MennyisegiEgyseg { Megnevezes = "teszt1" });
+                v.SaveChanges();
+
+                foreach (var entity in v.MennyisegiEgysegs)
+                {
+                    ret = entity.Megnevezes;
+                    v.MennyisegiEgysegs.Remove(entity);
+                }
+                v.SaveChanges();
+            }
+
+            return ret;
+        }
+
+    }
+}
